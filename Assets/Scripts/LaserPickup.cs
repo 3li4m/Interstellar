f9fe41a82a89c7d14411654ef57412ce36db2254
Elie <elie.ajaka87@hotmail.com>
2019-01-31 19:53:44 +1100

    sounds for main menu complete aswell as design.

---
+++ Assets/Scripts/LaserPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPickup : MonoBehaviour {
+
+	public GameObject laser;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			laser.SetActive (true);
+			other.gameObject.GetComponentInChildren<Laser> ().activeLaser = true;
+			Destroy (this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
